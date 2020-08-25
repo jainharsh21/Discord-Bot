e4925b6d12d98090e77d5c11578f9b7a9cfe17c4
jainharsh21 <jainharsh270@gmail.com>
2020-08-25 14:58:20 +0530

    replying to all users

--- src/bot.js
+++ src/bot.js
@@ -1,19 +1,38 @@
-require('dotenv').config()
-const { Client } = require('discord.js')
+require("dotenv").config();
+const { Client } = require("discord.js");
 
-const client = new Client()
+const client = new Client();
+const PREFIX = "$";
 
-client.on('ready',()=>{
-    console.log(`${client.user.tag} has logged in!`)
-})
+client.on("ready", () => {
+  console.log(`${client.user.tag} has logged in!`);
+});
 
-client.on('message',(message)=>{
-    if (message.author.bot) return;
-    console.log(`[${message.author.tag}]: ${message.content}`)
-    if(message.content === "hello"){
-        message.reply('Hello there! General Kenobi')
+client.on("message", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+
+    if(CMD_NAME === "kick"){
+        message.channel.send("Kicked the user"); 
     }
-})
+    console.log(CMD_NAME);
+    console.log(args);
+  }
 
-client.login(process.env.DISCORDJS_BOT_TOKEN)
+  
 
+  console.log(`[${message.author.tag}]: ${message.content}`);
+  if (message.content === "bachhi") {
+    message.reply("bacchhhhaaaaaaaaaa");
+  }
+  if (message.content === "baccha") {
+    message.reply("bacchhhhhhhhhiiiiiiiiiiiiiiiiii");
+  }
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
